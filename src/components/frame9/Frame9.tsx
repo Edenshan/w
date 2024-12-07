61aaf0c14cbf0797c59f08cccc4a3b482927bc31
Edenshan <eden.shan.w@gmail.com>
2024-12-07 15:08:56 +0800

    Create Frame9.tsx

---
+++ src/components/frame9/Frame9.tsx
@@ -0,0 +1,175 @@
+import { memo } from "react";
+import type { FC } from "react";
+
+import resets from "../_resets.module.css";
+import classes from "./Frame9.module.css";
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 81:2 */
+export const Frame9: FC<Props> = memo(function Frame9(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.section23}>
+        <div className={classes.starrySkyGalaxyTransperentBack}></div>
+        <div className={classes.section2}>
+          <div className={classes.container4}>
+            <div className={classes.text}>
+              <p className={classes.labelWrapper}>
+                is a practice that balances and harmonizes the flow of qi within
+                the body. It enhances the body&#39;s ability to gather, store,
+                and direct qi for health, healing, and spiritual growth through
+                exercises such as slow, controlled movements, breathing
+                techniques, meditation, and visualization.
+              </p>
+            </div>
+            <div className={classes.heading3}>
+              <p className={classes.labelWrapper2}>Qigong</p>
+            </div>
+          </div>
+        </div>
+        <div className={classes.section22}>
+          <div className={classes.container10}>
+            <div className={classes.text2}>
+              <div className={classes.textBlock}>
+                <p className={classes.labelWrapper3}>
+                  It connects all things in the universe
+                </p>
+              </div>
+              <div className={classes.textBlock2}>
+                <p className={classes.labelWrapper4}>
+                  It flows through all beings
+                </p>
+              </div>
+              <div className={classes.textBlock3}>
+                <p className={classes.labelWrapper5}>
+                  Sustaining life and maintaining balance in nature
+                </p>
+              </div>
+              <div className={classes.textBlock4}>
+                <p className={classes.labelWrapper6}></p>
+              </div>
+              <div className={classes.textBlock5}>
+                <p className={classes.labelWrapper7}></p>
+              </div>
+            </div>
+            <div className={classes.heading2}>
+              <p className={classes.labelWrapper8}>Qi is energy Qi is life</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.section45}>
+        <div className={classes.section5}></div>
+        <div className={classes.section4}>
+          <div className={classes.container17}>
+            <div className={classes.frame5}>
+              <div className={classes.text3}>
+                <p className={classes.labelWrapper9}>
+                  is a renowned Qigong master and one of the few people in the
+                  world able to emit Qi for therapeutic use. He is famous in
+                  Qigong circles for his abilities, including emitting Qi at a
+                  distance (Fa Gong), telekinesis, pyrokinesis, and
+                  electrogenesis, which he uses in medical treatments for
+                  various diseases. Master Zhou is also an experienced
+                  traditional Chinese medicine doctor, specializing in
+                  acupuncture. He has taught and treated patients worldwide,
+                  including in the United States, parts of Asia, and European
+                  countries like Italy and Sweden.
+                </p>
+              </div>
+            </div>
+            <div className={classes.frame6}>
+              <div className={classes.heading4}>
+                <p className={classes.labelWrapper10}>Master Zhou Gan sheng</p>
+              </div>
+            </div>
+          </div>
+          <div className={classes.frame4}>
+            <div className={classes._11}></div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.section6}>
+        <div className={classes.container23}>
+          <div className={classes.text4}>
+            <p className={classes.labelWrapper11}>
+              The master offers effective solutions for a wide range of health
+              issues, including both chronic and acute conditions. Pain
+              management conditions, such as periarthritis of the shoulder and
+              tenosynovitis, often improve rapidly with Qi treatment and
+              acupuncture. These techniques work by enhancing the flow of Qi, or
+              vital energy, within the body, reducing inflammation, and
+              promoting natural healing. Whether you&#39;re seeking relief from
+              stress-induced disorders, digestive issues, respiratory ailments,
+              neurological conditions, or other health concerns, Qi treatment
+              and TCM provide a holistic, non-invasive path to wellness.
+            </p>
+          </div>
+        </div>
+        <div className={classes.heading6}>
+          <p className={classes.labelWrapper12}>
+            Diseases that could be treated by Qi and TCM
+          </p>
+        </div>
+      </div>
+      <div className={classes.section7}>
+        <div className={classes.container30}>
+          <div className={classes.heading7}>
+            <p className={classes.labelWrapper14}>
+              Reach Out for More Info or Join the Next Clinic
+            </p>
+          </div>
+          <div className={classes.text5}>
+            <p className={classes.labelWrapper13}>
+              Master Zhou holds intensive clinics nearly every month in
+              different countries. Contact us for more information about the
+              clinics or if you&#39;re interested in seminars.
+            </p>
+          </div>
+        </div>
+        <div className={classes._81c845e30d2cb051a024db378f2106aa}></div>
+      </div>
+      <div className={classes.footer}>
+        <div className={classes.logoColumn}>
+          <div className={classes.frame11}>
+            <div className={classes.frame112}></div>
+          </div>
+          <div className={classes.logoText}>
+            <div className={classes.textBlock6}>© 2024 QiChi</div>
+            <div className={classes.textBlock7}>All rights reserved.</div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.frame15}>
+        <div className={classes.section1}>
+          <div className={classes.frame13}>
+            <div className={classes.bbe7c4cb1de129327d7463cc08012e}></div>
+          </div>
+          <div className={classes.frame12}>
+            <div className={classes.bbe7c4cb1de129327d7463cc08012e2}></div>
+          </div>
+          <div className={classes.container3}>
+            <div className={classes.heading1}>
+              <p className={classes.labelWrapper15}>
+                Heal with Qi and Traditional Chinese Medicine
+              </p>
+            </div>
+            <div className={classes.text6}>
+              <p className={classes.labelWrapper16}>
+                Experience the transformative power of ancient wisdom to achieve
+                balance, vitality, and holistic healing.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={classes.nav}>
+          <div className={classes.frame10}>
+            <div className={classes.frame113}></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
